6c72fceda3b3b8772fd9c6570dde9ff679990b5e
Daniele Vinci <vinci.dani@gmail.com>
2019-08-17 11:42:59 -0400

    banner component css

---
+++ src/components/globals/Banner.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import styled, { css, keyframes } from 'styled-components';
+import { setColor } from '../../utils/styles'
+
+const Banner = ({ className, greeting, title, text, children }) => {
+  return (
+    <div className={ className }>
+      <h1>
+        { greeting } <span>{ title }</span>{" "}
+      </h1>
+
+      <div className="info">
+        <p>{ text }</p>
+        { children }
+      </div>
+    </div>
+  )
+};
+
+const BannerWrapper = styled(Banner)`
+  background: rgba(0,0,0,0.7);
+  text-align: center;
+  padding: 60px 32px;
+  /* setRem*/
+  letter-spacing: 3px;
+  /* setLetterSpacing*/
+  color: ${ setColor.mainWhite };
+  h1{
+    text-transform: capitalize;
+    font-size: 48px;
+    /* setRem*/
+    color: ${ setColor.primaryColor };
+    span{
+      color: ${ setColor.mainWhite };
+    }
+  }
+
+  p{
+    width: 85%;
+    margin: 0 auto;
+  }
+
+  @media (min-width:768px) {
+    width: 70vw;
+    /* setBorder */
+    border: 6px solid ${ setColor.primaryColor };
+    p{
+      width: 75%;
+    }
+  }
+
+  h1{
+    /* Animations */
+  }
+
+  .info {
+    /* Animations */
+  }
+`;
+
+export default BannerWrapper;
--- src/components/homePageComponents/Header.js
+++ src/components/homePageComponents/Header.js
@@ -2,11 +2,16 @@ import React from 'react';
 import Hero from '../globals/Hero';
 import homeImg from '../../images/homeBcg.jpeg';
 // import aboutImg from '../../images/aboutBcg.jpeg'
+import Banner from '../globals/Banner';
 
 const Header = () => {
   return (
     <Hero img={ homeImg }>
-      <h1>Hello world</h1>
+      <Banner 
+        greeting="welcome to" 
+        title="beachwalk resort" 
+        text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, libero saepe ab necessitatibus dolores molestias."
+      />
     </Hero>
   )
 };

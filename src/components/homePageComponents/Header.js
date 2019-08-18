7181cfd79ee3a0076b165f70544e631e5577a6a5
Daniele Vinci <vinci.dani@gmail.com>
2019-08-18 11:34:59 -0400

    finished buttons

--- src/components/homePageComponents/Header.js
+++ src/components/homePageComponents/Header.js
@@ -3,6 +3,7 @@ import Hero from '../globals/Hero';
 import homeImg from '../../images/homeBcg.jpeg';
 // import aboutImg from '../../images/aboutBcg.jpeg'
 import Banner from '../globals/Banner';
+import { PrimaryBtn } from '../globals/Buttons';
 
 const Header = () => {
   return (
@@ -11,7 +12,15 @@ const Header = () => {
         greeting="welcome to" 
         title="beachwalk resort" 
         text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, libero saepe ab necessitatibus dolores molestias."
-      />
+      >
+        {/* <PrimaryBtn t="1rem">
+          view details
+        </PrimaryBtn> */}
+
+        <PrimaryBtn t="1rem" as="a" href="https://www.google.com" target="_blank">
+          view details
+        </PrimaryBtn>
+      </Banner>
     </Hero>
   )
 };

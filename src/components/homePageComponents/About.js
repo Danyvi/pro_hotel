60ab57b70057b2847e62d93ed39789258f6d1e1f
Daniele Vinci <vinci.dani@gmail.com>
2019-08-18 12:02:01 -0400

    section component and title components

---
+++ src/components/homePageComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Section from '../globals/Section';
+import Title from '../globals/Title';
+
+
+const About = () => {
+  return (
+    <Section>
+      <Title title="about us"></Title>
+    </Section>
+  )
+}
+
+export default About
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 // snippet to create functional component created with rafc
 import React from 'react';
 import Header from '../components/homePageComponents/Header';
+import About from '../components/homePageComponents/About';
 
 const Home = () => {
   return (
     <>
       <Header />
+      <About />
     </>
   )
 }
